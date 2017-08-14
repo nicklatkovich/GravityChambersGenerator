b7f35240d553580a6fad74f2b2c53adff5590fe8
NickLatkovich <nicklatkovich@gmail.com>
2017-08-14 14:12:42 +0300

    Converting Grid to String

--- GravityLabChamberGenerator/Grid.cs
+++ GravityLabChamberGenerator/Grid.cs
@@ -1,6 +1,11 @@
 ﻿namespace GravityLabChamberGenerator {
     class Grid<T> {
         private T[ ][ ] arr;
+        public Point Size {
+            get; protected set;
+        }
+        public uint Width => (uint)Size.X;
+        public uint Height => (uint)Size.Y;
 
         public Grid(int width, int height, T defaultValue) {
             arr = new T[width][ ];
@@ -10,6 +15,11 @@
                     this[i, j] = defaultValue;
                 }
             }
+            Size = new Point(width, height);
+        }
+
+        public bool HasCell(Point pos) {
+            return pos.X >= 0 && pos.X < Width && pos.Y >= 0 && pos.Y < Height;
         }
 
         public T this[Point p] {
@@ -28,6 +38,20 @@
             set {
                 arr[x][y] = value;
             }
+        }
+
+        public override string ToString( ) {
+            string result = "";
+            result += Width + '\n';
+            result += Height + '\n';
+            for (uint y = 0; y < Height; y++) {
+                for (uint x = 0; x < Width; x++) {
+                    // TODO: make converting for non one-symbols cells
+                    result += this[x, y];
+                }
+                result += '\n';
+            }
+            return result;
         }
     }
 }
