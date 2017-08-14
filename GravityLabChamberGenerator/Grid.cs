b9997149ef2a4464408e3ee2e57206f8914ec9c5
NickLatkovich <nicklatkovich@gmail.com>
2017-08-14 13:25:33 +0300

    Class Grid

---
+++ GravityLabChamberGenerator/Grid.cs
@@ -0,0 +1,33 @@
+﻿namespace GravityLabChamberGenerator {
+    class Grid<T> {
+        private T[ ][ ] arr;
+
+        public Grid(int width, int height, T defaultValue) {
+            arr = new T[width][ ];
+            for (uint i = 0; i < width; i++) {
+                arr[i] = new T[height];
+                for (uint j = 0; j < height; j++) {
+                    this[i, j] = defaultValue;
+                }
+            }
+        }
+
+        public T this[Point p] {
+            get {
+                return this[(uint)p.X, (uint)p.Y];
+            }
+            set {
+                this[(uint)p.X, (uint)p.Y] = value;
+            }
+        }
+
+        public T this[uint x, uint y] {
+            get {
+                return arr[x][y];
+            }
+            set {
+                arr[x][y] = value;
+            }
+        }
+    }
+}
