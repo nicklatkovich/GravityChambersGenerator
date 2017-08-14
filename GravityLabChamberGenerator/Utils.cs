63c15d4360a30e8640ab11b60e3bb86081a9ba0f
NickLatkovich <nicklatkovich@gmail.com>
2017-08-14 13:29:04 +0300

    Random generator

---
+++ GravityLabChamberGenerator/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GravityLabChamberGenerator {
+    static class Utils {
+        private static Random _rand = new Random( );
+
+        public static double Random( ) {
+            return _rand.NextDouble( );
+        }
+
+        public static uint URandom(uint x) {
+            return (uint)Math.Floor(Random( ) * x);
+        }
+    }
+}
